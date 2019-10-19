a39c43e1da5cfb14dc2359b47968a55c6426f97d
Pablo Cantón <pablocg1995@gmail.com>
2019-10-19 11:38:32 +0200

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -6,13 +6,16 @@ import Main from './components/main';
 import { Link } from 'react-router-dom';
 import '../node_modules/font-awesome/css/font-awesome.min.css';
 
-class App extends Component{
+class App extends React.Component{
+  componentDidMount() {
+    this._div.scrollTop = 0
+  }
   render() {
     return (
-    <div>
+    <div ref={(ref) => this._div = ref}>
     <Layout className="Body">
           <div  className="NavBar">
-            <Navigation >
+            <Navigation>
                 <Link to="/"><img className="Button HidePhone"
                 src={LogoPCBW} style={{height: '2.5em'}}/></Link>
 
